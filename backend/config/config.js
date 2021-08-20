028f09cf4c312c683ed3eb2dfc5398d54c078ac2
danmuttaqi <wildanmuttaqi@gmail.com>
2021-08-20 14:39:07 +0700

    config sequelize

---
+++ backend/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        username: "root",
+        password: "covid19#",
+        database: "db_covid19",
+        host: "mysql",
+        dialect: 'mysql',
+        key: 'covid19#12',
+        use_env_variable: 'DATABASE_URL'
+    }
+};
