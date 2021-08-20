a9bac1b6145ae490ec8d6f7576f32f42e2f9394a
danmuttaqi <wildanmuttaqi@gmail.com>
2021-08-20 14:37:28 +0700

    util setup

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var baseRouter = require('./routes/base');
 
 var app = express();
 
@@ -13,6 +15,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(helmet({contentSecurityPolicy: false,}))
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', baseRouter);
+app.use('/docs', express.static(path.join(__dirname,'/docs')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,7 +36,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'dev' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
