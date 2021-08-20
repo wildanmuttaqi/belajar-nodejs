8094759ebd7cfd3a832afe9695c95e89e9aaa0cb
danmuttaqi <wildanmuttaqi@gmail.com>
2021-08-20 14:40:15 +0700

    users model

---
+++ backend/models/users.js
@@ -0,0 +1,48 @@
+'use strict'
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER(4),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      set(val) {
+        const hash = bcrypt.hashSync(val, 10);
+        this.setDataValue('password', hash);
+      }
+    },
+    full_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    phone_number: {
+      type: DataTypes.STRING(40),
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    }
+  }, {
+    tableName: 'users',
+    timestamps: true,
+  });
+
+  return User;
+};
