65e36b2d6a094210df557c18d0944ccd1a20a7ce
danmuttaqi <wildanmuttaqi@gmail.com>
2021-08-21 23:05:20 +0700

    config production

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  // development: {
-  //   username: "root",
-  //   password: "covid19#",
-  //   database: "db_covid19",
-  //   host: "mysql",
-  //   dialect: 'mysql',
-  //   key: 'covid19#12',
-  //   use_env_variable: 'DATABASE_URL'
-  // },
   development: {
+    username: "root",
+    password: "covid19#",
+    database: "db_covid19",
+    host: "mysql",
+    dialect: 'mysql',
+    key: 'covid19#12',
+    use_env_variable: 'DATABASE_URL'
+  },
+  production: {
     username: "ybmbzgwfnwfuil",
     password: "55f6a55e23427f00edad319b41cbf0a07e4f0976d236157418679a39a499f5d5",
     port: "5432",
