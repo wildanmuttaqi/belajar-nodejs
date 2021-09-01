72ca4af0dd9f105e2d66be870122bd4e943596d3
danmuttaqi <wildanmuttaqi@gmail.com>
2021-09-01 18:24:00 +0700

    update locala.sh

--- local.sh
+++ local.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 set -e
 
-#set node_modules
-echo "Build Dependencies.."
-cd backend
-npm install
-
-#create-apidocs
-npm run apidocs
-
 #create volume
 docker volume create --name=covid19-db
 
 #run docker
+echo 'Build Docker...'
 docker-compose up
